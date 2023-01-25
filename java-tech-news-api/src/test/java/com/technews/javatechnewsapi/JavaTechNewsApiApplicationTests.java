a49effc3fe3b0d72c4c47fe7823364d080b82785
AliCelikay <alicelikay129@gmail.com>
2023-01-25 15:34:11 -0600

    initial commit

---
+++ java-tech-news-api/src/test/java/com/technews/javatechnewsapi/JavaTechNewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technews.javatechnewsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTechNewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
